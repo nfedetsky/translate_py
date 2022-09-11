22c9350e47b3063190ae56b43080399b48f78672
nicholas_fedetsky <nfedetsky@gmail.com>
2022-09-11 21:56:26 +0300

    0.03

--- translateText.py
+++ translateText.py
@@ -1,2 +1,9 @@
 import cv2
 import pytesseract
+import matplotlib.pyplot as plt
+from PIL import Image
+
+image = cv2.imread("D:\Repository\Python\translate_py\files\test.jpg")
+
+string = pytesseract.image_to_string(image)
+print(string)
