244e3db95ed88da940d6f1d5a1191c3ba19d90f8
nicholas_fedetsky <nfedetsky@gmail.com>
2022-09-13 00:33:41 +0300

    0.0.5

--- translateText.py
+++ translateText.py
@@ -3,16 +3,23 @@ import pytesseract
 import easyocr
 import matplotlib.pyplot as plt
 from PIL import Image
+from docx import Document
+from docx.shared import Pt
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
 img_file = 'D:/Repository/Python/translate_py/files/test.jpg'
-f_file = 'D:/Repository/Python/translate_py/files/t2.txt'
+
 img = Image.open(img_file)
 pytesseract.pytesseract.tesseract_cmd = r'C://Program Files/Tesseract-OCR/tesseract.exe'
 
 im_text = pytesseract.image_to_string(img, lang='rus')
 
-print(im_text)
+document = Document()
+document = Document('D:/Repository/Python/translate_py/files/tt_1.docx')
 
-f = open(f_file, 'a')
-f.write(im_text + '\n')
-f.close()
+style = document.styles['Normal']
+style.font.name = 'Times New Roman'
+style.font.size = Pt(14)
+par_1 = document.add_paragraph('Распознанный текст' + '\n')
+par_1.add_run(im_text)
+document.save('D:/Repository/Python/translate_py/files/tt_1.docx')
