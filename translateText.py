9c75eec77e7ce4e5de1b009d3fb67bf084abf0b4
nicholas_fedetsky <nfedetsky@gmail.com>
2022-09-12 10:42:28 +0300

    0.0.4.1

--- translateText.py
+++ translateText.py
@@ -1,9 +1,18 @@
 import cv2
 import pytesseract
+import easyocr
 import matplotlib.pyplot as plt
 from PIL import Image
 
-image = cv2.imread("D:\Repository\Python\translate_py\files\test.jpg")
+img_file = 'D:/Repository/Python/translate_py/files/test.jpg'
+f_file = 'D:/Repository/Python/translate_py/files/t2.txt'
+img = Image.open(img_file)
+pytesseract.pytesseract.tesseract_cmd = r'C://Program Files/Tesseract-OCR/tesseract.exe'
 
-string = pytesseract.image_to_string(image)
-print(string)
+im_text = pytesseract.image_to_string(img, lang='rus')
+
+print(im_text)
+
+f = open(f_file, 'a')
+f.write(im_text + '\n')
+f.close()
